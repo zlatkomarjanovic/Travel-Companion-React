7f6a9ff0afc14c6706b6362ad38115c4222c30d6
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-19 20:04:44 +0200

    Updates
    Updating this project but will have to put it on hold because google requires me to verify my account with credit card and I have no money so fuck that I guess.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'; 
+import {Paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+import useStyles from './Styles'
 
 const Map = () => {
+    const classes = useStyles(); 
+    const isMobile = useMediaQuery('(min-width:600px)'); 
+
+    const coordinates = { lat :0 , long:0}
+    const API_KEY =  "AIzaSyCKVsHCGY6B_vk_0YlNXFN05R6xI_SAQC4"
+
     return (
-        <div>
-           Map 
-        </div>
+        
+           <div className = {classes.mapContainer}>
+                <GoogleMapReact
+                    bootstrapURLKeys = {{key: API_KEY}}
+                    defaultCenter = {coordinates}
+                    center = {coordinates}
+                    defaultZoom = {14}
+                    margin={[50, 50, 50, 50]}
+                    options = {''}
+                    onChange = {''}
+                    onChildClick = {''}
+                >
+                    
+                </GoogleMapReact>
+           </div> 
+        
     )
 }
 
