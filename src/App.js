030670b90388a47d5fec2a1806e9ebcb1169f5e2
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-19 14:58:20 +0200

    aa
    aa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react'; 
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
 
 const App = () => {
     return (
