0a140f00caaf62db4ba24844eea7996ab1841ea6
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-08-20 22:24:43 +0200

    fgh
    fhg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+import { getPlacesData } from './api';
 
 const App = () => {
+	const [places, setPlaces] = useState([]);
+	const [coordinates, setCoordinates] = useState({});
+	const [bounds, setBounds] = useState(null);
+
+	useEffect(() => {
+		console.log(coordinates, bounds);
+		getPlacesData().then((data) => {
+			console.log(data);
+			setPlaces(data);
+		});
+	}, []);
+
 	return (
 		<>
 			<CssBaseline />
